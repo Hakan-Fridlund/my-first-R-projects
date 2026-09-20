63fce18c366c97d82b0deca55b4d718918bd7029
Hakan-Fridlund <garonneman79@gmail.com>
2026-09-20 10:52:44 +0200

    split lesson2 into lesson2 and lesson3, translated descriptions for about half lesson 2.

--- lesson2.R
+++ lesson2.R
@@ -1,20 +1,30 @@
-#   Övning: filtrera, gruppera, summera data.  DPLYR flights Tibble (dataframe)
-
+"""
+Assignment: filter, group, summerize data.  DPLYR flights Tibble (dataframe)
+"""
 pacman::p_load(nycflights13)
 library(nycflights13)
 library(tidyverse)
+
 ?flights
-view(flights) #bäst, men finns även glimpse() och print(flights, width = Inf)
+view(flights) # a couple of useful commands for viewing the datatable
+glimpse(flights)
+print(flights, width = Inf)
 class(flights)
 
-flights |> filter(month %in% c(1, 2)) # filtrerar rader som har månad 1 eller 2
-flights |> arrange(year, month, day, dep_time) # arrangerar lägsta år, sedan månad, dag och dep time. för INVERSE use ARRANGE(dsc(year))
-flights |> distinct()    #filtrerar bort duplicates
+flights |> filter(month %in% c(1, 2)) # filter rows that has month 1 or 2
+flights |> arrange(year, month, day, dep_time) # arranges lowest year, then month, day and dep time. for INVERSE use ARRANGE(dsc(year))
+flights |> distinct()    #filter away duplicates
 flights |> distinct(origin, dest)    # find unique origin and destination PAIRS
-flights |> count(origin, dest, sort = TRUE)   # räknar unique origin and destination PAIRS instead and sorts desc
+flights |> count(origin, dest, sort = TRUE)   #counts unique origin and destination PAIRS instead and sorts descending
 
-#ÖVNING 1 Had an arrival delay of two or more hours  2  Flew to Houston (IAH or HOU) 3 Were operated by United, American, or Delta
-#4 Departed in summer (July, August, and September) 5 Arrived more than two hours late but didn’t leave late 6 Were delayed by at least an hour, but made up over 30 minutes in flight
+"""
+#Assignment. Filter: 1 Had an arrival delay of two or more hours  
+2  Flew to Houston (IAH or HOU) 
+3 Were operated by United, American, or Delta
+#4 Departed in summer (July, August, and September) 
+5 Arrived more than two hours late but didn’t leave late 
+6 Were delayed by at least an hour, but made up over 30 minutes in flight
+"""
 
 x <- flights |> filter(arr_delay >= 120)
 x
@@ -22,7 +32,7 @@ y <- flights |> filter(dest %in% c("IAH", "HOU"))
 y
 z <- flights |> filter(carrier %in% c("UA", "AA", "DL"))
 z
-a <- flights |> filter(month == 7 | month == 8 | month == 9) # annat sätt att skriva, omständigare än %in%
+a <- flights |> filter(month == 7 | month == 8 | month == 9) # another way to write, more complicated than %in%
 a                       
 b <- flights |> filter(arr_delay > 120 & dep_delay == 0) 
 b
@@ -86,113 +96,3 @@ x <- flights |>
   filter(n>50)               # tar bort dom PAIRS av destinationer/carriers som flugits färre än 50 ggr
 x
 
-#     GGPLOT
-library(tidyverse)
-library(palmerpenguins)
-library(ggthemes)
-penguins
-glimpse(penguins)
-view(penguins)
-?penguins
-
-ggplot(  # version 1 har color = species globalt
-  data = penguins,
-  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
-) +
-  geom_point() +   # skapar punkt scatterplot
-  geom_smooth(method = "lm")  # skapar en smooth linje med metoden linear model
-
-
-ggplot(    # version 2 har color = species lokalt i scatterpointen
-  data = penguins,
-  mapping = aes(x = flipper_length_mm, y = body_mass_g)
-) +
-  geom_point(mapping = aes(color = species, shape = species)) +   #sätter olika former för varje species punkt
-  geom_smooth(method = "lm") +
-  labs(                                           
-    title = "Body mass and flipper length",
-    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
-    x = "Flipper length (mm)", y = "Body mass (g)",
-    color = "Species", shape = "Species"
-  ) +
-  scale_color_colorblind()
-
-# Make a scatterplot of bill_depth_mm vs. bill_length_mm. That is, make a scatterplot with bill_depth_mm on the y-axis and bill_length_mm on the x-axis. Describe the relationship between these two variables.
-
-ggplot(
-  data = penguins,
-  mapping = aes(x =bill_length_mm, y = bill_depth_mm)
-) +
-  geom_point(mapping = aes(color = species, shape = species, na.rm = TRUE)) +
-  scale_color_colorblind()
-
-# What happens if you make a scatterplot of species vs. bill_depth_mm? What might be a better choice of geom? svar boxplot
-ggplot(
-  data = penguins,
-  mapping = aes(x =species, y = bill_depth_mm)
-) +
-  geom_boxplot(mapping = aes(color = species)) +
-  labs(
-    caption = "Data come from the palmerpenguins package.",
-    title = "Penguin Beak depths"
-  ) +
-  scale_color_colorblind() 
-
-#bättre syntax för plotting med pipes och data först, sedan mapping via aes
-penguins |> 
-  ggplot(aes(x = flipper_length_mm, y = body_mass_g)) + 
-  geom_point()
-
-ggplot(penguins, aes(x = fct_infreq(species))) +    # bar chart om du bara har t.ex. species. fct_infreq sorterar fallande
-  geom_bar()
-
-ggplot(penguins, aes(x = body_mass_g)) +     # histogram
-  geom_histogram(binwidth = 200)
-
-ggplot(penguins, aes(x = body_mass_g)) +    # density plot fungerar som ett histogram fast linje
-  geom_density()
-
-
-#Make a bar plot of species of penguins, where you assign species to the y aesthetic. How is this plot different? sideways
-penguins |> 
-  ggplot(aes(y = species)) +
-  geom_bar()
-
-# Make a histogram of the carat variable in the diamonds dataset that is available when you load the tidyverse package. 
-# Experiment with different binwidths. What binwidth reveals the most interesting patterns?
-?diamonds
-view(diamonds)
-diamonds |> 
-  ggplot(aes(x = carat)) +
-  geom_histogram(binwidth = 0.3)
-
-penguins |>    # fylld density plot, alpha sätter genomskinligheten
-  ggplot(aes(x = body_mass_g, color = species, fill = species)) +
-  geom_density(alpha = 0.5)
-
-ggplot(penguins, aes(x = island, fill = species)) +   # visar antalet species på varje ö som fill
-  geom_bar()   #lägg till detta: (position = "fill") för att få barsen alla till 100 och visa % istället,  använd labs(y = "proportion") för att döpa om y-axeln
-
-ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +  # facet_wrap skapar en subplot för varje ö (kategori)
-  geom_point(aes(color = species, shape = species)) +
-  facet_wrap(~island)
-
-# Make a scatterplot of bill_depth_mm vs. bill_length_mm and color the points by species. 
-# What does adding coloring by species reveal about the relationship between these two variables? What about faceting by species?
-
-penguins |> 
-  ggplot(aes(x = bill_depth_mm, y = bill_length_mm, color = species)) +
-  geom_point() +
-  facet_wrap(~species)
-
-ggplot(
-  data = penguins,
-  mapping = aes(
-    x = bill_length_mm, y = bill_depth_mm, 
-    color = species, shape = species
-  )
-) +
-  geom_point()
-ggsave(filename = "penguin-plot.png")
-
-?ggsave
